92ec2047c9af94c2469d7f811f950d526ff5c2c3
Sarvani Kandukuri <sarvanik60@gmail.com>
2022-10-10 23:01:14 +0530

    Updated Home page

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+const Home = () => {
+    return(
+        <>
+            <div>
+                <div className="p-16">
+                    <div className="grid grid-cols-2 gap-10 items-center">
+                        <h1 className="text-red text-left text-5xl">Welcome to Exclusive Arya Vysya Matrimonial Website</h1>
+
+                        <img src="/rings3.png" alt="rings-pic" className="w-full"/>
+                    </div>
+
+                    <div className="pt-20">
+                        <h1 className="text-red text-2xl font-bold">ABOUT WEDDING:</h1>
+                        
+                        <p className="text-xl pt-4">A wedding is a civil or religious ceremony which celebrates the beginning of a marriage. Wedding traditions and customs vary greatly between cultures, ethnic groups, religions, countries, and social classes. Wedding is an opportunity to perform a traditional ceremony and celebrate with friends and family.</p>
+                        
+                        <p className="text-xl pt-4">While there are many festival-related rituals in Hinduism, vivaah (wedding) is the most extensive personal ritual an adult Hindu undertakes in his or her life. Typical Hindu families spend significant effort and financial resources to prepare and celebrate weddings. The rituals and process of a Hindu wedding vary depending on region of India, local adaptations, resources of the family and preferences of the bride and the groom. Nevertheless, there are a few key rituals common in Hindu weddings - Kanyadaan, Panigrahana, and Saptapadi; these are respectively, gifting away of daughter by the father, voluntarily holding hand near the fire to signify impending union, and taking seven steps before fire with each step including a set of mutual vows. After the seventh step and vows of Saptapadi, the couple is legally husband and wife.</p>
+
+                        <img src="/cow-image.jpg" alt="cow-muni" className="py-10"/>.
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
